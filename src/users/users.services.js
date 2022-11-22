a27ad1de27458ba5f1662d195f80aa15d25daa15
Stefa093 <estefaniasalazar093@gmail.com>
2022-11-21 20:29:41 -0500

    first commit

---
+++ src/users/users.services.js
@@ -0,0 +1,43 @@
+const userControllers = require("./users.controllers");
+
+const getAllUsers = (req, res) => {
+  const data = userControllers.findAllUsers();
+  res.status(200).json(data);
+};
+
+const getUserById = (req, res) => {
+  const id = req.params.id;
+  const data = userControllers.findUserById(id);
+  if (data) {
+    res.status(200).json(data);
+  } else {
+    res.status(404).json({
+      message: "Invalid ID",
+    });
+  }
+};
+
+const postNewUser = (req, res) => {
+  const { first_name, last_name, email, password, birthday } = req.body;
+  if (first_name && last_name && email && password && birthday) {
+    const data = userControllers.createNewUser({ first_name, last_name, email, password, birthday });
+    res.status(201).json(data);
+  } else {
+    res.status(400).json({
+      message: "Invalid Data",
+      fields: { 
+        first_name: 'string',
+        last_name: 'string',
+        email: 'string',
+        password: 'string',
+        birthday: 'YYYY/MM/DD'
+    },
+    });
+  }
+};
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    postNewUser,
+}
